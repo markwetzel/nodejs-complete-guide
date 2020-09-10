d52e2340e16a3d5daf941f0d13ed5b6f7801ee02
Mark Wetzel <mark@markwetzel.com>
2020-09-10 13:40:38 -0400

    Convert PUG to handlebars and remove PUG files.

--- routes/admin.ts
+++ routes/admin.ts
@@ -15,6 +15,10 @@ router.get('/add-product', (req, res, next) => {
   res.render('add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+    // layout: false,
   });
 });
 
